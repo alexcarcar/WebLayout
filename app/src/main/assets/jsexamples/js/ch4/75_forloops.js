dc5d9cd4fe4838409ae0f3fe8e6cadd5bd1568e6
alex <alexcarcar@yahoo.com>
2017-10-31 06:12:30 -0700

    Added for loop examples

---
+++ app/src/main/assets/jsexamples/js/ch4/75_forloops.js
@@ -0,0 +1,30 @@
+console.log("----- for loops (p. 75) ");
+
+const player = {name: 'Alex', rank: 'Midshipman', age: 25};
+for (let prop in player) {
+	if (!player.hasOwnProperty(prop)) continue;
+	console.log(prop + ": " + player[prop]);
+}
+
+function rand(m, n) {
+	return m + Math.floor((n-m+1)*Math.random());
+}
+
+function randFace() {
+	return ["crown", "anchor", "heart", "spade", "club", "diamond"][rand(0,5)];
+}
+
+const hand = [randFace(), randFace(), randFace()];
+for (let face of hand) {
+	console.log(`You rolled...${face}!`);
+}
+
+/*
+----- for loops (p. 75)
+name: Alex
+rank: Midshipman
+age: 25
+You rolled...anchor!
+You rolled...crown!
+You rolled...spade!
+ */
