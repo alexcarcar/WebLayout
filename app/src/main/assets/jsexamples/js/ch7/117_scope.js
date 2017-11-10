3e8f5864cc58ca7e70b44bcb85ca2b4759473446
alexcarcar <alexcarcar@yahoo.com>
2017-11-10 12:45:58 -0800

    Added definitions and examples of visibility, scope, lexical scoping for chapter 7

--- app/src/main/assets/jsexamples/js/ch7/117_scope.js
+++ app/src/main/assets/jsexamples/js/ch7/117_scope.js
@@ -1,6 +1,38 @@
 console.log("=== scope (p 117)");
 
+// Example 1: function scope
 function fScope(x) {
 	return x + 3; // scope of x is the function fScope
 }
 console.log(fScope(5), typeof x); // x is not defined
+
+// Example 2: lexical versus dynamic scoping
+function fLexical1() {
+	console.log('one');
+}
+
+function fLexical2() {
+	console.log('two');
+}
+fLexical2();
+fLexical1();
+fLexical2();
+
+// Example 3: lexical scoping
+const xLexical = 3;
+function fLexical3() {
+	console.log(xLexical); // this will work
+	console.log(typeof yLexical);
+}
+const yLexical = 3;
+fLexical3();
+
+/*
+=== scope (p 117)
+8 'undefined'
+two
+one
+two
+3
+number
+*/
