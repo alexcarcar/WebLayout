63b7177029a1c347b7852daa6b0574b42aede652
alexcarcar <alexcarcar@yahoo.com>
2017-11-27 07:17:06 -0800

    Added examples of block-scoped variables and IIFEs (chapter 13)

--- app/src/main/assets/jsexamples/js/ch13/180_subroutines.js
+++ app/src/main/assets/jsexamples/js/ch13/180_subroutines.js
@@ -49,3 +49,43 @@ console.log(getNextRainbowColor());
 console.log(getNextRainbowColor());
 console.log(getNextRainbowColor());
 console.log(getNextRainbowColor());
+
+console.log("=== pure functions (p 187)");
+function getRainbowIterator() {
+	const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+	let colorIndex = -1;
+	return {
+		next() {
+			colorIndex++;
+			return {value: colors[colorIndex%colors.length], done:false};
+		}
+	}
+}
+const rainbowIterator = getRainbowIterator();
+const rainbowArray = [];
+for (i=0;i<10;i++) {
+	rainbowArray.push(rainbowIterator.next().value);
+}
+console.log(rainbowArray.join("|"));
+/*
+=== Functions as Subroutines (p 180)
+2017 is NOT a leap year.
+2016 IS a leap year
+2018 is NOT a leap year.
+[ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+[ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+=== Functions as ... Functions
+red
+green
+blue
+red
+green
+=== Putting external variables in a closure (p 186)
+orange
+yellow
+purple
+orange
+yellow
+=== pure functions (p 187)
+red|orange|yellow|green|blue|red|orange|yellow|green|blue
+*/
---
+++ app/src/main/assets/jsexamples/js/ch13/189_asynchronous.js
@@ -0,0 +1,8 @@
+console.log("=== IIFEs and Asynchronous Code (p 189)");
+setTimeout(function() {console.log("hello");}, 1500);
+for(let i=5; i>=0; i--) {
+	setTimeout(function() {
+		console.log(i===0 ? "go!" : i);
+	}, (5-i)*1000);
+}
+
