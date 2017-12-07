117dd6182fe66487123efff873f538bfc310c055
alex <alexcarcar@yahoo.com>
2017-12-07 15:04:54 -0800

    Added more examples of dates (ch 15)

--- app/src/main/assets/jsexamples/js/ch15/222_moment.js
+++ app/src/main/assets/jsexamples/js/ch15/222_moment.js
@@ -7,9 +7,11 @@ d = moment.tz([2016, 3, 27, 9, 19], 'America/Los_Angeles').toDate();
 console.log("LA", d);
 
 console.log("=== Transmitting Dates (p 223)");
-/*
-=== Moment.js (p 222)
-UTC 2016-05-27T00:00:00.000Z
-LA 2016-04-27T16:19:00.000Z
-=== Transmitting Dates (p 223)
-*/
+const before = {d: new Date()};
+console.log(before.d instanceof Date);
+const json = JSON.stringify(before);
+const after = JSON.parse(json);
+console.log(json, after);
+console.log(after.d, after.d instanceof Date, typeof after.d);
+after.d = new Date(after.d);
+console.log(after.d, after.d instanceof Date, typeof after.d);
