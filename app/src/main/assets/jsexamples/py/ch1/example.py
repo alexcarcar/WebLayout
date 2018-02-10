29513e41f31c7aac0c8fc2f0d211c08fe86c12d1
alexcarcar <alexcarcar@yahoo.com>
2018-02-10 07:36:17 -0800

    Added chapter 1 of python programming

---
+++ app/src/main/assets/jsexamples/py/ch1/example.py
@@ -0,0 +1,16 @@
+print ('hello world')
+
+miles = 10.0 / 1.61
+hours = (43.0*60.0+30.0)/3600.0
+print (miles, 'mi')
+print (hours, 'h')
+print (miles/hours, "mph")
+exit()
+
+/*
+% python example.py
+hello world
+(6.211180124223602, 'mi')
+(0.725, 'h')
+(8.567144998929106, 'mph')
+*/
