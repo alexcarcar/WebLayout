86349a54b04255ac1b573472b82bb75568d18020
alexcarcar <alexcarcar@yahoo.com>
2018-03-03 07:52:28 -0800

    Completed chapter 9 exercises (age, mileage puzzles)

--- app/src/main/assets/jsexamples/py/ch9/exercises.py
+++ app/src/main/assets/jsexamples/py/ch9/exercises.py
@@ -75,35 +75,154 @@ def is_abecedarian(word):
 print is_abecedarian("Abc")			# True
 print is_abecedarian("cat")			# False
 
-# Exercise 9-7: three consecutive letters
-def three_consecutive_letters(word):
-	s = 1; prev = "-"; n = 0
+# Exercise 9-7: k consecutive repetitions
+def consecutive_repetitions(word):
+	firstLetter = True
+	consecutive = False
+	repetition = False
+	max = 0
+	m = 0
 	for c in word:
-		print s, c, prev, n
-		if s == 1: # first letter
-			prev = c; n = 0; s = 2
+		if firstLetter:
+			previous = c
+			firstLetter = False
 			continue
+		if c == previous:
+			if not consecutive:
+				consecutive = True
+			consecutive = True
+		else:
+			if consecutive:
+				m = m + 1
+			else:
+				if m > max:
+					max = m
+				m = 0
+			consecutive = False
+		previous = c
+	if consecutive:
+		m = m + 1
+	if m > max:
+		max = m
+	return max
 
-		if s == 2: # second letter
-			if c == prev:
-				s = 3
-				n = n + 1
-				continue;
-			if n == 3: return True;
-			n = 0
+word = "aapplleeddd"
+print word, consecutive_repetitions(word)			# 5
+
+fin = open('words.txt')
+for line in fin:
+	word = line.strip()
+	if consecutive_repetitions(word) >= 3:
+		print word
+fin.close()
+# bookkeeper
+# bookkeepers
+# bookkeeping
+# bookkeepings
+
+# Exercise 9-8
+# Find some mileage x, so that (starting at 300000)
+# 1. x      last 4 digits is a palendrone
+# 2. x+1    last 5 digits is a palendrone
+# 3. x+2    middle 4 digits is a palendrone
+# 4. x+3    all 6 digits is a palendrone 
+def palendrone(x):
+	word = str(x)
+	reverse = word[::-1]
+	return word == reverse
+
+def print_condition(x):
+	s = str(x)
+	l = len(s)
+	if not l == 6: return
+	print x, "last 4 digits", s[2:6]
+	x = x + 1
+	s = str(x)
+	print x, "last 5 digits", s[1:6]
+	x = x + 1
+	s = str(x)
+	print x, "middle 4 digits", s[1:5]
+	x = x + 1
+	s = str(x)
+	print x, "all digits", s
+
+def test_condition(x):
+	s = str(x)
+	l = len(s)
+	if not l == 6: return False
+	if not palendrone(s[2:6]):
+		return False
+	x = x + 1
+	s = str(x)
+	if not palendrone(s[1:6]):
+		return False
+	x = x + 1
+	s = str(x)
+	if not palendrone(s[1:5]):
+		return False
+	x = x + 1
+	s = str(x)
+	if not palendrone(s):
+		return False
+	return True
+
+x = 123456
+print_condition(x)
+# 123456 last 4 digits 3456
+# 123457 last 5 digits 23457
+# 123458 middle 4 digits 2345
+# 123459 all digits 123459
+print test_condition(x)				# False
+
+x = 100000
+while x < 300000:
+	if test_condition(x):
+		print_condition(x)
+		print ""
+	x = x + 1
+
+# There are two solutions:
+# 198888 last 4 digits 8888
+# 198889 last 5 digits 98889
+# 198890 middle 4 digits 9889
+# 198891 all digits 198891
+#
+# 199999 last 4 digits 9999
+# 200000 last 5 digits 00000
+# 200001 middle 4 digits 0000
+# 200002 all digits 200002
+
+# Exercise 9-9
+# for example: son = 37, mom = 73
+# reversible 6 times so far
+# will happen two more times
+
+def is_reverse(x,y):
+	word = str(x)
+	reverse = str(y)
+	reverse = reverse[::-1]
+	return word == reverse
+print is_reverse(37, 73)		# True
+print is_reverse(37, 72)		# False
+
+ages = {}
+pairs = {}
+for x in range(1,99):
+	for y in range(1,99):
+		if x>=y:
 			continue
+		a = str(x).zfill(2)
+		b = str(y).zfill(2)
+		if is_reverse(a,b):
+			diff = abs(int(a)-int(b))
+			ages[diff] = ages.get(diff, 0) + 1
+			pairs[diff] = pairs.get(diff, "") + "("+str(a)+","+str(b)+") "
+for x in ages:
+	if ages[x] >= 8:
+		print pairs[x]
+#(01,10) (12,21) (23,32) (34,43) (45,54) (56,65) (67,76) (78,87) (89,98)
+#(02,20) (13,31) (24,42) (35,53) (46,64) (57,75) (68,86) (79,97)
 
-		if s == 3: # repeated letter
-			if c == prev: continue
-			s = 1; continue
-
-	return False
-
-three_consecutive_letters("aapplleedd")
-
-# fin = open('words.txt')
-# for line in fin:
-# 	word = line.strip()
-# 	if three_consecutive_letters(word):
-# 		print word
-# fin.close()
+# First solution mother is too young
+# Son is now 57 and mother is 75, see solution 2
+#(02,20) (13,31) (24,42) (35,53) (46,64) (57,75) (68,86) (79,97)
