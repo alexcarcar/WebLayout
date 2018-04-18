33913d2a1e635f64282e1651f85aa6993bcfb665
alex <alexcarcar@yahoo.com>
2018-04-18 16:42:40 -0700

    Updated multiple regression (statistics) problem

--- app/src/main/assets/jsexamples/js/misc/java/04-statistics/09-multiple-regression/Solution.java
+++ app/src/main/assets/jsexamples/js/misc/java/04-statistics/09-multiple-regression/Solution.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -53,32 +52,135 @@ the feature sets.
 */
 public class Solution {
 
-    public static double[][] transposeMatrix(double [][] m){
-        double[][] temp = new double[m[0].length][m.length];
-        for (int i = 0; i < m.length; i++)
-            for (int j = 0; j < m[0].length; j++)
-                temp[j][i] = m[i][j];
-        return temp;
+    private static double determinant(double[][] matrix) {
+        if (matrix.length != matrix[0].length)
+            throw new IllegalStateException("invalid dimensions");
+
+        if (matrix.length == 2)
+            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+
+        double det = 0;
+        for (int i = 0; i < matrix[0].length; i++)
+            det += Math.pow(-1, i) * matrix[0][i]
+                    * determinant(minor(matrix, 0, i));
+        return det;
     }
+
+    private static double[][] inverse(double[][] matrix) {
+        double[][] inverse = new double[matrix.length][matrix.length];
+
+        // minors and cofactors
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix[i].length; j++)
+                inverse[i][j] = Math.pow(-1, i + j)
+                        * determinant(minor(matrix, i, j));
+
+        // adjugate and determinant
+        double det = 1.0 / determinant(matrix);
+        for (int i = 0; i < inverse.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                double temp = inverse[i][j];
+                inverse[i][j] = inverse[j][i] * det;
+                inverse[j][i] = temp * det;
+            }
+        }
+
+        return inverse;
+    }
+
+    private static double[][] minor(double[][] matrix, int row, int column) {
+        double[][] minor = new double[matrix.length - 1][matrix.length - 1];
+
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; i != row && j < matrix[i].length; j++)
+                if (j != column)
+                    minor[i < row ? i : i - 1][j < column ? j : j - 1] = matrix[i][j];
+        return minor;
+    }
+
+    private static double[][] multiply(double[][] a, double[][] b) {
+        if (a[0].length != b.length)
+            throw new IllegalStateException("invalid dimensions");
+
+        double[][] matrix = new double[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                double sum = 0;
+                for (int k = 0; k < a[i].length; k++)
+                    sum += a[i][k] * b[k][j];
+                matrix[i][j] = sum;
+            }
+        }
+
+        return matrix;
+    }
+
+    private static double[][] rref(double[][] matrix) {
+        double[][] rref = new double[matrix.length][];
+        for (int i = 0; i < matrix.length; i++)
+            rref[i] = Arrays.copyOf(matrix[i], matrix[i].length);
+
+        int r = 0;
+        for (int c = 0; c < rref[0].length && r < rref.length; c++) {
+            int j = r;
+            for (int i = r + 1; i < rref.length; i++)
+                if (Math.abs(rref[i][c]) > Math.abs(rref[j][c]))
+                    j = i;
+            if (Math.abs(rref[j][c]) < 0.00001)
+                continue;
+
+            double[] temp = rref[j];
+            rref[j] = rref[r];
+            rref[r] = temp;
+
+            double s = 1.0 / rref[r][c];
+            for (j = 0; j < rref[0].length; j++)
+                rref[r][j] *= s;
+            for (int i = 0; i < rref.length; i++) {
+                if (i != r) {
+                    double t = rref[i][c];
+                    for (j = 0; j < rref[0].length; j++)
+                        rref[i][j] -= t * rref[r][j];
+                }
+            }
+            r++;
+        }
+
+        return rref;
+    }
+
+    private static double[][] transpose(double[][] matrix) {
+        double[][] transpose = new double[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix[i].length; j++)
+                transpose[j][i] = matrix[i][j];
+        return transpose;
+    }
+
+    // ----
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int m = in.nextInt(); // # of observed features
         int n = in.nextInt(); // # of of feature sets
 
-        double[][] X = new double[n][m+1];
-        double[][] Xt = transposeMatrix(X);
+        double[][] X = new double[n][m + 1];
         double[] Y = new double[n];
         for (int i = 0; i < n; i++) {
             X[i][0] = 1;
             for (int j = 0; j < m; j++) {
-                X[i][j+1] = in.nextDouble();
+                X[i][j + 1] = in.nextDouble();
             }
             Y[i] = in.nextDouble();
         }
-        System.out.println(Arrays.deepToString(X).replace(']', '\n'));
-        System.out.println(Arrays.deepToString(Xt));
-        System.out.println(Y);
+        double[][] Xt = transpose(X);
+        double[][] XtX = multiply(Xt, X);
+        double[][] inv_XtX = inverse(XtX);
+        double[][] inv_XtX_Xt = multiply(inv_XtX, Xt);
+        double[][] B = multiply(inv_XtX_Xt, Y);
+
+        System.out.println(Arrays.deepToString(B).replace(']', '\n'));
 
         int q = in.nextInt(); // # of queries
         double[][] b = new double[q][m];
