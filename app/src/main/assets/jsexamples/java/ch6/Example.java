9ea50f01dab144789f2187728f1f7d6a35505914
alexcarcar <alexcarcar@yahoo.com>
2018-05-15 07:39:17 -0700

    Added content for Chapter 6: A Closer Look at Methods and Classes

--- app/src/main/assets/jsexamples/java/ch6/Example.java
+++ app/src/main/assets/jsexamples/java/ch6/Example.java
@@ -203,6 +203,122 @@ class Summmation {
 	}
 }
 
+// A simple example of recursion. (p 204)
+class Factorial {
+	// This is a recursive function.
+	int factR(int n) {
+		int result;
+		if (n==1) return 1;
+		result = factR(n-1)*n;
+		return result;
+	}
+
+	// This is an iterative equivalent.
+	int factI(int n) {
+		int t, result;
+		result = 1;
+		for (t=1; t<=n; t++) {
+			result *= t;
+		}
+		return result;
+	}
+}
+
+// Use a static variable. (p 207)
+class StaticDemo {
+	int x; // a normal instance variable
+	static int y; // a static variable (there is only one copy for all objects)
+	int sum() {
+		return x+y;
+	}
+}
+
+// Use a static method. (p 208)
+class StaticMeth {
+	static int val = 1024; // a static variable
+
+	// a static method
+	static int valDiv2() {
+		return val/2;
+	}
+
+	/*
+	int denom = 3;
+	static int valDivDenom() {
+		return val/denom; // won't compile
+	} */
+	// Example.java:247: error: non-static variable denom cannot be referenced
+	// from a static context
+    //            return val/denom; // won't compile
+    //                       ^
+}
+
+// Use a static block. (p 209)
+class StaticBlock {
+	static double rootOf2;
+	static double rootOf3;
+
+	static { // This block is executed when the class is loaded.
+		System.out.println("Inside static block.");
+		rootOf2 = Math.sqrt(2.0);
+		rootOf3 = Math.sqrt(3.0);
+	}
+
+	StaticBlock(String msg) {
+		System.out.println(msg);
+	}
+	// Inside static block.
+	// Inside Constructor
+	// Square root of 2 is 1.4142135623730951
+	// Square root of 3 is 1.7320508075688772
+}
+
+// Use an inner class. (p 213)
+class Outer {
+	int nums[];
+	Outer(int n[]) {
+		nums = n;
+	}
+
+	void analyze() {
+		Inner inOb = new Inner();
+		System.out.println("Minimum: " + inOb.min());
+		System.out.println("Maximum: " + inOb.max());
+		System.out.println("Average: " + inOb.avg());
+	}
+
+	// This is an inner class.
+	class Inner {
+		int min() {
+			int m = nums[0];
+			for (int i=1; i<nums.length; i++) if (nums[i]<m) m = nums[i];
+			return m;
+		}
+		int max() {
+			int m = nums[0];
+			for (int i=1; i<nums.length; i++) if (nums[i]>m) m = nums[i];
+			return m;
+		}
+		double avg() {
+			double a = 0;
+			for (int i=0; i<nums.length; i++) a += nums[i];
+			return a/nums.length;
+		}
+	}
+}
+
+// Demonstrate variable-length arguments. (p 217)
+class VarArgs {
+	// vaTest() uses a vararg.
+	static void vaTest(int ... v) {
+		System.out.println("Number of args: " + v.length);
+		System.out.println("Contents: ");
+		for (int i=0; i<v.length; i++) {
+			System.out.println(" arg "+i+": "+v[i]);
+		}
+	}
+}
+
 class Example {
     static void accessDemo() {
         MyClass ob = new MyClass();
@@ -383,6 +499,77 @@ class Example {
 		// s2.sum: 15
     }
 
+    static void recursion() {
+    	Factorial f = new Factorial();
+
+    	System.out.println("Factorials using recursive method.");
+    	System.out.println("3! = " + f.factR(3));
+    	System.out.println("4! = " + f.factR(4));
+    	System.out.println("5! = " + f.factR(5));
+    	System.out.println("Factorials using iterative method.");
+    	System.out.println("3! = " + f.factI(3));
+    	System.out.println("4! = " + f.factI(4));
+    	System.out.println("5! = " + f.factI(5));
+		// Factorials using recursive method.
+		// 3! = 6
+		// 4! = 24
+		// 5! = 120
+		// Factorials using iterative method.
+		// 3! = 6
+		// 4! = 24
+		// 5! = 120
+    }
+
+    static void sDemo() {
+    	StaticDemo ob1 = new StaticDemo();
+    	StaticDemo ob2 = new StaticDemo();
+
+    	// Each object has its own copy of an instance variable.
+    	ob1.x = 10;
+    	ob2.x = 20;
+    	System.out.println("ob1.x: "+ob1.x); // 10
+    	System.out.println("ob2.x: "+ob2.x); // 20
+    	StaticDemo.y = 19;
+    	System.out.println("ob1.sum(): "+ob1.sum()); // 29
+    	System.out.println("ob2.sum(): "+ob2.sum()); // 39
+    	StaticDemo.y = 100;
+    	System.out.println("ob1.sum(): "+ob1.sum()); // 110
+    	System.out.println("ob2.sum(): "+ob2.sum()); // 120
+		// ob1.x: 10
+		// ob2.x: 20
+		// ob1.sum(): 29
+		// ob2.sum(): 39
+		// ob1.sum(): 110
+		// ob2.sum(): 120
+    }
+
+    static void sDemo2() {
+    	System.out.println("val is "+StaticMeth.val); // 1024
+    	System.out.println("StaticMeth.valDiv2() is "+StaticMeth.valDiv2()); // 512
+    	StaticMeth.val = 4;
+    	System.out.println("val is "+StaticMeth.val); // 4
+    	System.out.println("StaticMeth.valDiv2() is "+StaticMeth.valDiv2()); // 2
+		// val is 1024
+		// StaticMeth.valDiv2() is 512
+		// val is 4
+		// StaticMeth.valDiv2() is 2
+    }
+
+    static void sDemo3() {
+    	StaticBlock ob = new StaticBlock("Inside Constructor");
+    	System.out.println("Square root of 2 is "+StaticBlock.rootOf2);
+    	System.out.println("Square root of 3 is "+StaticBlock.rootOf3);
+    }
+
+    static void nestedClassDemo() {
+    	int x[] = {3,2,1,5,6,9,7,8};
+    	Outer outOb = new Outer(x);
+    	outOb.analyze();
+		// Minimum: 1
+		// Maximum: 9
+		// Average: 5.125
+    }
+
     public static void main(String[] args) {
         accessDemo();
         fsDemo();
@@ -395,5 +582,62 @@ class Example {
         typeConv();
         overloadConsDemo();
         sumDemo();
+        recursion();
+        sDemo();
+        sDemo2();
+        sDemo3();
+        nestedClassDemo();
+
+        // An inner class version of ShowBits.
+        class ShowBits {
+        	int numbits;
+        	ShowBits(int n) {
+        		numbits = n;
+        	}
+        	void show(long val) {
+        		long mask = 1;
+        		mask <<= numbits-1;
+        		int spacer = 0;
+        		for(;mask!=0;mask>>>=1) {
+        			if((val&mask)!=0) System.out.print("1");
+        			else System.out.print("0");
+        			spacer++;
+        			if((spacer%8)==0) {
+        				System.out.print(" ");
+        				spacer = 0;
+        			}
+        		}
+        		System.out.println();
+        	}
+        }
+        for(byte b=0; b<10; b++) {
+        	ShowBits byteval = new ShowBits(8);
+        	System.out.print(b + " in binary: ");
+        	byteval.show(b);
+        }
+        // 0 in binary: 00000000 
+		// 1 in binary: 00000001 
+		// 2 in binary: 00000010 
+		// 3 in binary: 00000011 
+		// 4 in binary: 00000100 
+		// 5 in binary: 00000101 
+		// 6 in binary: 00000110 
+		// 7 in binary: 00000111 
+		// 8 in binary: 00001000 
+		// 9 in binary: 00001001 
+
+        VarArgs.vaTest(10);
+		// Number of args: 1
+		// Contents:
+		//  arg 0: 10
+        VarArgs.vaTest(1,2,3);
+		// Number of args: 3
+		// Contents:
+		//  arg 0: 1
+		//  arg 1: 2
+		//  arg 2: 3
+        VarArgs.vaTest();
+		// Number of args: 0
+		// Contents:
     }
 }
