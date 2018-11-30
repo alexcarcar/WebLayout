1ed45de5d341ff612141cef878c815644536f037
alexcarcar <alexcarcar@yahoo.com>
2018-11-30 06:50:21 -0800

    Completed chapter 8: Arrays and Array Processing

--- app/src/main/assets/jsexamples/lj/ch8/example8-3.js
+++ app/src/main/assets/jsexamples/lj/ch8/example8-3.js
@@ -70,3 +70,111 @@ function cardToString(c) {
 }
 
 filterExample1();
+
+// ================================================================================
+// Array Magic: reduce
+
+// - Where "map" transforms each element in the array,
+// - "reduce" transforms the entire array (i.e. sum, average)
+// - "reduce" allows you to provide a callback function, with:
+// -> first element is an "accumulator" which is what the array is reducing to
+// -> second element - current array element
+// -> third element - current index
+// -> forth element - the array itself
+// - in addition to the callback function, reduce can take a optional initial value
+
+reduceExamples();
+function reduceExamples() {
+	const arr = [5, 7, 2, 4];
+	const sum = arr.reduce((a,x) => a+=x, 0); // a initially is 0
+	const sum2 = arr.reduce((a,x) => a+=x); // here a is initially undefined
+	console.log(sum, sum2);
+}
+
+// For example, if you have an array of strings you can create an object accumulator
+// to divide the strings into alphabetic arrays
+alphabeticArrays();
+function alphabeticArrays() {
+	const words = [
+		"Beachball", "Rodeo", "Angel", "Aardvark", "Xylophone", "November",
+		"Chocolate", "Papaya", "Uniform", "Joker", "Clover", "Bali"];
+
+	const alphabetical = words.reduce((a,x) => {
+		if (!a[x[0]]) a[x[0]] = [];
+		a[x[0]].push(x);
+		return a;
+	}, {});
+	console.log(alphabetical);
+	// { B: [ 'Beachball', 'Bali' ],
+	//   R: [ 'Rodeo' ],
+	//   A: [ 'Angel', 'Aardvark' ],
+	//   X: [ 'Xylophone' ],
+	//   N: [ 'November' ],
+	//   C: [ 'Chocolate', 'Clover' ],
+	//   P: [ 'Papaya' ],
+	//   U: [ 'Uniform' ],
+	//   J: [ 'Joker' ] }
+}
+
+// For example, reduce to calculate mean and variation
+meanAndVaration();
+function meanAndVaration() {
+	const data = [3.3, 5, 7.2, 12, 4, 6, 10.3];
+	// Using Donald Knuth's Algorithm
+	const stats = data.reduce((a,x) => {
+		a.N++;
+		let delta = x-a.mean;
+		a.mean += delta/a.N;
+		a.M2 += delta*(x - a.mean);
+		return a;
+	}, {N:0, mean:0, M2:0});
+	if (stats.N>2) {
+		stats.variance = stats.M2/(stats.N-1);
+		stats.stdev = Math.sqrt(stats.variance);
+	}
+	console.log(stats);
+	// { N: 7,
+	//   mean: 6.828571428571428,
+	//   M2: 63.41428571428572,
+	//   variance: 10.56904761904762,
+	//   stdev: 3.2510071699471257 }
+}
+
+// For example, using a string accumulator to get a single string
+// containing all words longer than six letters.
+longerThan6();
+function longerThan6() {
+	const words = [
+		"Beachball", "Rodeo", "Angel", "Aardvark", "Xylophone", "November",
+		"Chocolate", "Papaya", "Uniform", "Joker", "Clover", "Bali"];
+
+	const longWords = words.reduce((a,w)=>w.length>6?a+" "+w:a ,"").trim();
+	console.log(longWords);
+	// Beachball Aardvark Xylophone November Chocolate Uniform
+}
+
+// =========================================================================
+arrayMethodsWithDeletedElements(); // map skips deleted elements
+function arrayMethodsWithDeletedElements() {
+	const arr = Array(10).map(function(x) {return 5});
+	console.log(arr); // [ , , , , , , , , ,  ]
+
+	const arr2 = [1, 2, 3, 4, 5];
+	delete arr2[2];
+	console.log(arr2.map(x=>0)); // [ 0, 0, , 0, 0 ]
+}
+
+// =========================================================================
+// Array.prototype.join takes a single argument (comma if you omit it)
+stringJoining();
+function stringJoining() {
+	const arr = [1, null, "hello", "world", true, undefined];
+	delete arr[3];
+	console.log(arr.join()); // 1,,hello,,true,
+	console.log(arr.join('')); // 1hellotrue
+	console.log(arr.join(' -- ')); // 1 --  -- hello --  -- true --
+
+	const attributes = ["Nimble", "Perceptive", "Generous"];
+	const html = '<ul><li>' + attributes.join('</li><li>') + '</li></ul>';
+	console.log(html); // <ul><li>Nimble</li><li>Perceptive</li><li>Generous</li></ul>
+}
