e1cb12ece847d1d846c122d6c744368e7ecd212a
alexcarcar <alexcarcar@yahoo.com>
2018-12-05 07:17:10 -0800

    Added python examples to chapter 1: the way of the program

--- app/src/main/assets/jsexamples/py/ch1/example.py
+++ app/src/main/assets/jsexamples/py/ch1/example.py
@@ -1,14 +1,18 @@
-print ('hello world')
+# Exercise 1-2
+# Visit http://python.org
 
+# Exercise 1-3
+help('print') # no documentation found for 'print'
+print ('hello world') # hello world
+
+# Example 1-4
 miles = 10.0 / 1.61
 hours = (43.0*60.0+30.0)/3600.0
 print (miles, 'mi')
 print (hours, 'h')
 print (miles/hours, "mph")
-exit()
-
-# % python example.py
-# hello world
 # (6.211180124223602, 'mi')
 # (0.725, 'h')
 # (8.567144998929106, 'mph')
+
+exit()
